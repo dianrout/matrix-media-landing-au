cc811822973150873821eb50143462681bd9e914
Nguyễn Khắc Duy <diannguyen@Mac-mini-cua-Nguyen.local>
2023-12-22 13:48:57 +0700

    ui header

--- components/Header/HeaderMobile.tsx
+++ components/Header/HeaderMobile.tsx
@@ -82,7 +82,7 @@ const HeaderMobile = ({ id, visible, setVisible }: any) => {
             >
               <path
                 d="M14.0001 22.0001C15.0507 22.0001 16.091 21.7932 17.0616 21.3911C18.0322 20.9891 18.9141 20.3998 19.657 19.657C20.3998 18.9141 20.9891 18.0322 21.3911 17.0616C21.7932 16.091 22.0001 15.0507 22.0001 14.0001C22.0001 12.9495 21.7932 11.9092 21.3911 10.9386C20.9891 9.96803 20.3998 9.08612 19.657 8.34325C18.9141 7.60038 18.0322 7.0111 17.0616 6.60907C16.091 6.20703 15.0507 6.0001 14.0001 6.0001C11.8784 6.0001 9.84354 6.84296 8.34325 8.34325C6.84296 9.84354 6.0001 11.8784 6.0001 14.0001C6.0001 16.1218 6.84296 18.1567 8.34325 19.657C9.84354 21.1572 11.8784 22.0001 14.0001 22.0001ZM22.4268 20.5414L27.2001 25.3148C27.3274 25.4379 27.4288 25.5851 27.4986 25.7478C27.5683 25.9105 27.605 26.0855 27.6064 26.2625C27.6078 26.4396 27.574 26.6151 27.5068 26.7789C27.4397 26.9427 27.3405 27.0915 27.2153 27.2166C27.09 27.3417 26.9411 27.4406 26.7771 27.5076C26.6132 27.5745 26.4376 27.6081 26.2606 27.6064C26.0836 27.6048 25.9086 27.5679 25.746 27.4979C25.5834 27.4279 25.4363 27.3262 25.3134 27.1988L20.5401 22.4254C18.3967 24.0892 15.6998 24.8737 12.9984 24.619C10.2971 24.3644 7.79424 23.0899 5.9995 21.0548C4.20476 19.0198 3.25301 16.3774 3.338 13.6653C3.42299 10.9533 4.53833 8.3756 6.45696 6.45696C8.3756 4.53833 10.9533 3.42299 13.6653 3.338C16.3774 3.25301 19.0198 4.20476 21.0548 5.9995C23.0899 7.79424 24.3644 10.2971 24.619 12.9984C24.8737 15.6998 24.0892 18.3967 22.4254 20.5401L22.4268 20.5414Z"
-                fill="#EDEDED"
+                fill="#2F2F2F"
               />
             </svg>
           </div>
@@ -97,15 +97,15 @@ const HeaderMobile = ({ id, visible, setVisible }: any) => {
             >
               <path
                 d="M21 8.25H3C2.59 8.25 2.25 7.79667 2.25 7.25C2.25 6.70333 2.59 6.25 3 6.25H21C21.41 6.25 21.75 6.70333 21.75 7.25C21.75 7.79667 21.41 8.25 21 8.25Z"
-                fill="#EDEDED"
+                fill="#2F2F2F"
               />
               <path
                 d="M21 13.25H3C2.59 13.25 2.25 12.7967 2.25 12.25C2.25 11.7033 2.59 11.25 3 11.25H21C21.41 11.25 21.75 11.7033 21.75 12.25C21.75 12.7967 21.41 13.25 21 13.25Z"
-                fill="#EDEDED"
+                fill="#2F2F2F"
               />
               <path
                 d="M21 18.25H3C2.59 18.25 2.25 17.7967 2.25 17.25C2.25 16.7033 2.59 16.25 3 16.25H21C21.41 16.25 21.75 16.7033 21.75 17.25C21.75 17.7967 21.41 18.25 21 18.25Z"
-                fill="#EDEDED"
+                fill="#2F2F2F"
               />
             </svg>
           </div>
@@ -129,9 +129,9 @@ const HeaderMobile = ({ id, visible, setVisible }: any) => {
               <img
                 src={Logo.src}
                 alt="logo"
-                className=""
-                width={32}
-                height={32}
+                className="h-[50px] w-auto"
+                // width={50}
+                height={50}
                 style={{
                   objectFit: "cover"
                 }}
@@ -142,7 +142,7 @@ const HeaderMobile = ({ id, visible, setVisible }: any) => {
                 className="bg-transparent leading-[20px]  block outline-none focus:outline-none"
                 onClick={onClose}
               >
-                <IconClose fill="#616161" />
+                <IconClose fill="#2F2F2F" />
               </span>
             </button>
           </div>
@@ -152,7 +152,7 @@ const HeaderMobile = ({ id, visible, setVisible }: any) => {
               {routes.map((item, index) => {
                 return (
                   <li
-                    className="header__item text-text-main w-fit px-[16px] py-[8px] cursor-pointer hover:text-button-red Mulish-500 text-[18px] leading-[150%] mt-1 pl-[8px]"
+                    className="header__item uppercase text-[#2F2F2F] w-fit cursor-pointer hover:text-secondary Mulish-500 text-[16px] leading-[150%] mt-5"
                     onClick={() => {
                       router.push(item.router)
                       onClose()
@@ -163,18 +163,36 @@ const HeaderMobile = ({ id, visible, setVisible }: any) => {
                     key={index}
                   >
                     {item.name}
+                    <p className="header__item_border"></p>
                   </li>
                 )
               })}
             </ul>
-            <ButtonComponent
-              className="uppercase w-full mt-auto mb-0"
-              onSubmit={() => {
+            <div
+              onClick={() => {
                 onClose()
               }}
+              className="px-[24px] flex justify-center mt-auto mb-0 py-[12px] rounded-[44px] bg-[#EBF8F3] flex gap-[8px] text-[16px] leading-[24px] text-secondary border-secondary border border-secondary cursor-pointer"
             >
-              {t("common.contact_us")}
-            </ButtonComponent>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="25"
+                viewBox="0 0 24 25"
+                fill="none"
+              >
+                <path
+                  d="M20.9712 8.5C20.9712 8.5 16.95 13.5 12 13.5C7.05 13.5 3.02878 8.5 3.02878 8.5M7.8 19.5H16.2C17.8802 19.5 18.7202 19.5 19.362 19.173C19.9265 18.8854 20.3854 18.4265 20.673 17.862C21 17.2202 21 16.3802 21 14.7V10.3C21 8.61984 21 7.77976 20.673 7.13803C20.3854 6.57354 19.9265 6.1146 19.362 5.82698C18.7202 5.5 17.8802 5.5 16.2 5.5H7.8C6.11984 5.5 5.27976 5.5 4.63803 5.82698C4.07354 6.1146 3.6146 6.57354 3.32698 7.13803C3 7.77976 3 8.61984 3 10.3V14.7C3 16.3802 3 17.2202 3.32698 17.862C3.6146 18.4265 4.07354 18.8854 4.63803 19.173C5.27976 19.5 6.11984 19.5 7.8 19.5Z"
+                  stroke="#2D7A56"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+              <p className="text-[16px] uppercase Mulish-500">
+                {t("common.contact_us")}
+              </p>
+            </div>
           </div>
         </div>
       </Drawer>
--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -78,14 +78,14 @@ const Header = () => {
       }`}
     >
       {!isTablet ? (
-        <div className="m-auto hidden sd:flex px-[40px] 2xl:px-[80px] items-center justify-between">
-          <div className="header-left flex items-center gap-[48px]">
+        <div className="m-auto hidden sd:flex justify-between px-[40px] 2xl:px-[80px] items-center">
+          <div className="flex items-center gap-[48px]">
             <div
               className="header-center flex-1 flex items-center justify-center cursor-pointer"
               onClick={() => router.push("/")}
             >
               <img
-                src={navbar || other ? Logo.src : LogoLight.src}
+                src={Logo.src}
                 alt="logo"
                 className="h-[50px] w-auto"
                 // width={50}
@@ -95,26 +95,28 @@ const Header = () => {
                 }}
               />
             </div>
-            <ul className="flex flex-wrap gap-[2px] items-center">
-              {routes.map((item, index) => {
-                return (
-                  <li
-                    className="header__item text-[#E0E0E0] w-fit px-[16px] py-[8px] cursor-pointer hover:text-button-red Mulish-500 text-[18px] leading-[150%] mt-1 text-menu"
-                    onClick={() => router.push(item.router)}
-                    data-activeid={
-                      router.pathname === item.router ? "active" : ""
-                    }
-                    key={index}
-                  >
-                    {item.name}
-                  </li>
-                )
-              })}
-            </ul>
           </div>
 
-          <div className="flex items-center gap-[32px] flex-1 justify-end">
-            <div className="text-menu cursor-pointer">
+          <ul className="flex flex-wrap gap-[2px] items-center">
+            {routes.map((item, index) => {
+              return (
+                <li
+                  className="header__item text-[#83878D] w-fit cursor-pointer hover:text-secondary Mulish-600 text-[16px] mx-[25px] uppercase leading-[150%] text-menu"
+                  onClick={() => router.push(item.router)}
+                  data-activeid={
+                    router.pathname === item.router ? "active" : ""
+                  }
+                  key={index}
+                >
+                  {item.name}
+                  <p className="header__item_border"></p>
+                </li>
+              )
+            })}
+          </ul>
+
+          <div className="flex items-center gap-[32px] justify-end">
+            <div className="text-menu cursor-pointer bg-[#2d7a56] w-[50px] h-[50px] flex items-center justify-center rounded-[25px]">
               <svg
                 width="32"
                 height="32"
@@ -124,11 +126,10 @@ const Header = () => {
               >
                 <path
                   d="M14.0001 22.0001C15.0507 22.0001 16.091 21.7932 17.0616 21.3911C18.0322 20.9891 18.9141 20.3998 19.657 19.657C20.3998 18.9141 20.9891 18.0322 21.3911 17.0616C21.7932 16.091 22.0001 15.0507 22.0001 14.0001C22.0001 12.9495 21.7932 11.9092 21.3911 10.9386C20.9891 9.96803 20.3998 9.08612 19.657 8.34325C18.9141 7.60038 18.0322 7.0111 17.0616 6.60907C16.091 6.20703 15.0507 6.0001 14.0001 6.0001C11.8784 6.0001 9.84354 6.84296 8.34325 8.34325C6.84296 9.84354 6.0001 11.8784 6.0001 14.0001C6.0001 16.1218 6.84296 18.1567 8.34325 19.657C9.84354 21.1572 11.8784 22.0001 14.0001 22.0001ZM22.4268 20.5414L27.2001 25.3148C27.3274 25.4379 27.4288 25.5851 27.4986 25.7478C27.5683 25.9105 27.605 26.0855 27.6064 26.2625C27.6078 26.4396 27.574 26.6151 27.5068 26.7789C27.4397 26.9427 27.3405 27.0915 27.2153 27.2166C27.09 27.3417 26.9411 27.4406 26.7771 27.5076C26.6132 27.5745 26.4376 27.6081 26.2606 27.6064C26.0836 27.6048 25.9086 27.5679 25.746 27.4979C25.5834 27.4279 25.4363 27.3262 25.3134 27.1988L20.5401 22.4254C18.3967 24.0892 15.6998 24.8737 12.9984 24.619C10.2971 24.3644 7.79424 23.0899 5.9995 21.0548C4.20476 19.0198 3.25301 16.3774 3.338 13.6653C3.42299 10.9533 4.53833 8.3756 6.45696 6.45696C8.3756 4.53833 10.9533 3.42299 13.6653 3.338C16.3774 3.25301 19.0198 4.20476 21.0548 5.9995C23.0899 7.79424 24.3644 10.2971 24.619 12.9984C24.8737 15.6998 24.0892 18.3967 22.4254 20.5401L22.4268 20.5414Z"
-                  fill="#EDEDED"
+                  fill="#fff"
                 />
               </svg>
             </div>
-            {/* <SelectLanguage /> */}
             <Link
               activeClass="active"
               to="contactUs"
@@ -140,9 +141,26 @@ const Header = () => {
               isDynamic={true}
               ignoreCancelEvents={false}
             >
-              <ButtonComponent className="uppercase w-fit" onSubmit={() => {}}>
-                {t("common.contact_us")}
-              </ButtonComponent>
+              <div className="px-[24px] py-[12px] rounded-[44px] bg-[#EBF8F3] flex gap-[8px] text-[16px] leading-[24px] text-secondary border-secondary border border-secondary cursor-pointer">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="25"
+                  viewBox="0 0 24 25"
+                  fill="none"
+                >
+                  <path
+                    d="M20.9712 8.5C20.9712 8.5 16.95 13.5 12 13.5C7.05 13.5 3.02878 8.5 3.02878 8.5M7.8 19.5H16.2C17.8802 19.5 18.7202 19.5 19.362 19.173C19.9265 18.8854 20.3854 18.4265 20.673 17.862C21 17.2202 21 16.3802 21 14.7V10.3C21 8.61984 21 7.77976 20.673 7.13803C20.3854 6.57354 19.9265 6.1146 19.362 5.82698C18.7202 5.5 17.8802 5.5 16.2 5.5H7.8C6.11984 5.5 5.27976 5.5 4.63803 5.82698C4.07354 6.1146 3.6146 6.57354 3.32698 7.13803C3 7.77976 3 8.61984 3 10.3V14.7C3 16.3802 3 17.2202 3.32698 17.862C3.6146 18.4265 4.07354 18.8854 4.63803 19.173C5.27976 19.5 6.11984 19.5 7.8 19.5Z"
+                    stroke="#2D7A56"
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+                <p className="text-[16px] uppercase Mulish-500">
+                  {t("common.contact_us")}
+                </p>
+              </div>
             </Link>
           </div>
         </div>
